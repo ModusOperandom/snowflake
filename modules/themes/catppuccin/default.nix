32fca96583d97d794fddf70517d9eed88a56b74e
Icy-Thought <icy-thought@pm.me>
2024-12-07 18:24:36 +1030

    re-enable themes in vscode modules, start adding in vscodium features

--- modules/themes/catppuccin/default.nix
+++ modules/themes/catppuccin/default.nix
@@ -7,7 +7,8 @@ let
   inherit (lib.strings) concatMapStringsSep;
 
   cfg = config.modules.themes;
-in {
+in
+{
   config = mkIf (cfg.active == "catppuccin") (mkMerge [
     {
       modules.themes = {
@@ -100,7 +101,7 @@ in {
             light = "catppuccin"; # TODO apply frappe flavour
           };
           vscode = {
-            dark = "Catppuccin";
+            dark = "Catppuccin Mocha";
             light = "Quiet Light";
             extension = {
               name = "catppuccin-vsc";
@@ -123,166 +124,170 @@ in {
       hm.programs.rofi = {
         extraConfig = {
           icon-theme = let inherit (cfg.iconTheme) name; in "${name}";
-          font = let inherit (cfg.font.sans) family weight size;
-          in "${family} ${weight} ${toString size}";
+          font =
+            let inherit (cfg.font.sans) family weight size;
+            in "${family} ${weight} ${toString size}";
         };
 
-        theme = let
-          inherit (config.hm.lib.formats.rasi) mkLiteral;
-          inherit (cfg.colors.rofi) bg fg ribbon selected transparent urgent;
+        theme =
+          let
+            inherit (config.hm.lib.formats.rasi) mkLiteral;
+            inherit (cfg.colors.rofi) bg fg ribbon selected transparent urgent;
+          in
+          {
+            "*" = {
+              fg = mkLiteral "${fg}";
+              bg = mkLiteral "${bg.main}";
+              bg-alt = mkLiteral "${bg.alt}";
+              bg-bar = mkLiteral "${bg.bar}";
+
+              outer-ribbon = mkLiteral "${ribbon.outer}";
+              inner-ribbon = mkLiteral "${ribbon.inner}";
+              selected = mkLiteral "${selected}";
+              urgent = mkLiteral "${urgent}";
+              transparent = mkLiteral "${transparent}";
+            };
+
+            "window" = {
+              transparency = "real";
+              background-color = mkLiteral "@bg";
+              text-color = mkLiteral "@fg";
+              border = mkLiteral "0% 0% 0% 1.5%";
+              border-color = mkLiteral "@outer-ribbon";
+              border-radius = mkLiteral "0% 0% 0% 2.5%";
+              height = mkLiteral "54.50%";
+              width = mkLiteral "43%";
+              location = mkLiteral "center";
+              x-offset = 0;
+              y-offset = 0;
+            };
+
+            "prompt" = {
+              enabled = true;
+              padding = mkLiteral "0% 1% 0% 0%";
+              background-color = mkLiteral "@bg-bar";
+              text-color = mkLiteral "@fg";
+            };
+
+            "entry" = {
+              background-color = mkLiteral "@bg-bar";
+              text-color = mkLiteral "@fg";
+              placeholder-color = mkLiteral "@fg";
+              expand = true;
+              horizontal-align = 0;
+              placeholder = "Search";
+              padding = mkLiteral "0.15% 0% 0% 0%";
+              blink = true;
+            };
+
+            "inputbar" = {
+              children = mkLiteral "[ prompt, entry ]";
+              background-color = mkLiteral "@bg-bar";
+              text-color = mkLiteral "@fg";
+              expand = false;
+              border = mkLiteral "0% 0% 0.3% 0.2%";
+              border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
+              border-color = mkLiteral "@inner-ribbon";
+              margin = mkLiteral "0% 17% 0% 0%";
+              padding = mkLiteral "1%";
+              position = mkLiteral "center";
+            };
+
+            "listview" = {
+              background-color = mkLiteral "@bg";
+              columns = 5;
+              spacing = mkLiteral "1%";
+              cycle = false;
+              dynamic = true;
+              layout = mkLiteral "vertical";
+            };
+
+            "mainbox" = {
+              background-color = mkLiteral "@bg";
+              border = mkLiteral "0% 0% 0% 1.5%";
+              border-radius = mkLiteral "0% 0% 0% 2.5%";
+              border-color = mkLiteral "@inner-ribbon";
+              children = mkLiteral "[ inputbar, listview ]";
+              spacing = mkLiteral "3%";
+              padding = mkLiteral "2.5% 2% 2.5% 2%";
+            };
+
+            "element" = {
+              background-color = mkLiteral "@bg-bar";
+              text-color = mkLiteral "@fg";
+              orientation = mkLiteral "vertical";
+              border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
+              padding = mkLiteral "2% 0% 2% 0%";
+            };
+
+            "element-icon" = {
+              background-color = mkLiteral "@transparent";
+              text-color = mkLiteral "inherit";
+              horizontal-align = "0.5";
+              vertical-align = "0.5";
+              size = mkLiteral "64px";
+              border = mkLiteral "0px";
+            };
+
+            "element-text" = {
+              background-color = mkLiteral "@transparent";
+              text-color = mkLiteral "inherit";
+              expand = true;
+              horizontal-align = mkLiteral "0.5";
+              vertical-align = mkLiteral "0.5";
+              margin = mkLiteral "0.5% 1% 0% 1%";
+            };
+
+            "element normal.urgent, element alternate.urgent" = {
+              background-color = mkLiteral "@urgent";
+              text-color = mkLiteral "@fg";
+              border-radius = mkLiteral "1%";
+            };
+
+            "element normal.active, element alternate.active" = {
+              background-color = mkLiteral "@bg-alt";
+              text-color = mkLiteral "@fg";
+            };
+
+            "element selected" = {
+              background-color = mkLiteral "@selected";
+              text-color = mkLiteral "@bg";
+              border = mkLiteral "0% 0% 0.3% 0.2%";
+              border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
+              border-color = mkLiteral "@inner-ribbon";
+            };
+
+            "element selected.urgent" = {
+              background-color = mkLiteral "@urgent";
+              text-color = mkLiteral "@fg";
+            };
+
+            "element selected.active" = {
+              background-color = mkLiteral "@bg-alt";
+              color = mkLiteral "@fg";
+            };
+          };
+      };
+
+      hm.programs.sioyek.config =
+        let inherit (cfg.font) mono sans;
         in {
-          "*" = {
-            fg = mkLiteral "${fg}";
-            bg = mkLiteral "${bg.main}";
-            bg-alt = mkLiteral "${bg.alt}";
-            bg-bar = mkLiteral "${bg.bar}";
+          "custom_background_color " = "0.12 0.11 0.18";
+          "custom_text_color " = "0.85 0.88 0.93";
 
-            outer-ribbon = mkLiteral "${ribbon.outer}";
-            inner-ribbon = mkLiteral "${ribbon.inner}";
-            selected = mkLiteral "${selected}";
-            urgent = mkLiteral "${urgent}";
-            transparent = mkLiteral "${transparent}";
-          };
+          "text_highlight_color" = "0.85 0.88 0.93";
+          "visual_mark_color" = "0.27 0.28 0.35 1.0";
+          "search_highlight_color" = "0.95 0.55 0.66";
+          "link_highlight_color" = "0.59 0.80 0.98";
+          "synctex_highlight_color" = "0.96 0.88 0.86";
 
-          "window" = {
-            transparency = "real";
-            background-color = mkLiteral "@bg";
-            text-color = mkLiteral "@fg";
-            border = mkLiteral "0% 0% 0% 1.5%";
-            border-color = mkLiteral "@outer-ribbon";
-            border-radius = mkLiteral "0% 0% 0% 2.5%";
-            height = mkLiteral "54.50%";
-            width = mkLiteral "43%";
-            location = mkLiteral "center";
-            x-offset = 0;
-            y-offset = 0;
-          };
+          "page_separator_width" = "2";
+          "page_separator_color" = "0.95 0.80 0.80";
+          "status_bar_color" = "0.19 0.20 0.27";
 
-          "prompt" = {
-            enabled = true;
-            padding = mkLiteral "0% 1% 0% 0%";
-            background-color = mkLiteral "@bg-bar";
-            text-color = mkLiteral "@fg";
-          };
-
-          "entry" = {
-            background-color = mkLiteral "@bg-bar";
-            text-color = mkLiteral "@fg";
-            placeholder-color = mkLiteral "@fg";
-            expand = true;
-            horizontal-align = 0;
-            placeholder = "Search";
-            padding = mkLiteral "0.15% 0% 0% 0%";
-            blink = true;
-          };
-
-          "inputbar" = {
-            children = mkLiteral "[ prompt, entry ]";
-            background-color = mkLiteral "@bg-bar";
-            text-color = mkLiteral "@fg";
-            expand = false;
-            border = mkLiteral "0% 0% 0.3% 0.2%";
-            border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
-            border-color = mkLiteral "@inner-ribbon";
-            margin = mkLiteral "0% 17% 0% 0%";
-            padding = mkLiteral "1%";
-            position = mkLiteral "center";
-          };
-
-          "listview" = {
-            background-color = mkLiteral "@bg";
-            columns = 5;
-            spacing = mkLiteral "1%";
-            cycle = false;
-            dynamic = true;
-            layout = mkLiteral "vertical";
-          };
-
-          "mainbox" = {
-            background-color = mkLiteral "@bg";
-            border = mkLiteral "0% 0% 0% 1.5%";
-            border-radius = mkLiteral "0% 0% 0% 2.5%";
-            border-color = mkLiteral "@inner-ribbon";
-            children = mkLiteral "[ inputbar, listview ]";
-            spacing = mkLiteral "3%";
-            padding = mkLiteral "2.5% 2% 2.5% 2%";
-          };
-
-          "element" = {
-            background-color = mkLiteral "@bg-bar";
-            text-color = mkLiteral "@fg";
-            orientation = mkLiteral "vertical";
-            border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
-            padding = mkLiteral "2% 0% 2% 0%";
-          };
-
-          "element-icon" = {
-            background-color = mkLiteral "@transparent";
-            text-color = mkLiteral "inherit";
-            horizontal-align = "0.5";
-            vertical-align = "0.5";
-            size = mkLiteral "64px";
-            border = mkLiteral "0px";
-          };
-
-          "element-text" = {
-            background-color = mkLiteral "@transparent";
-            text-color = mkLiteral "inherit";
-            expand = true;
-            horizontal-align = mkLiteral "0.5";
-            vertical-align = mkLiteral "0.5";
-            margin = mkLiteral "0.5% 1% 0% 1%";
-          };
-
-          "element normal.urgent, element alternate.urgent" = {
-            background-color = mkLiteral "@urgent";
-            text-color = mkLiteral "@fg";
-            border-radius = mkLiteral "1%";
-          };
-
-          "element normal.active, element alternate.active" = {
-            background-color = mkLiteral "@bg-alt";
-            text-color = mkLiteral "@fg";
-          };
-
-          "element selected" = {
-            background-color = mkLiteral "@selected";
-            text-color = mkLiteral "@bg";
-            border = mkLiteral "0% 0% 0.3% 0.2%";
-            border-radius = mkLiteral "1.5% 1.0% 1.5% 1.5%";
-            border-color = mkLiteral "@inner-ribbon";
-          };
-
-          "element selected.urgent" = {
-            background-color = mkLiteral "@urgent";
-            text-color = mkLiteral "@fg";
-          };
-
-          "element selected.active" = {
-            background-color = mkLiteral "@bg-alt";
-            color = mkLiteral "@fg";
-          };
+          "font_size" = "${toString sans.size}";
+          "ui_font" = "${mono.family} ${mono.weight}";
         };
-      };
-
-      hm.programs.sioyek.config = let inherit (cfg.font) mono sans;
-      in {
-        "custom_background_color " = "0.12 0.11 0.18";
-        "custom_text_color " = "0.85 0.88 0.93";
-
-        "text_highlight_color" = "0.85 0.88 0.93";
-        "visual_mark_color" = "0.27 0.28 0.35 1.0";
-        "search_highlight_color" = "0.95 0.55 0.66";
-        "link_highlight_color" = "0.59 0.80 0.98";
-        "synctex_highlight_color" = "0.96 0.88 0.86";
-
-        "page_separator_width" = "2";
-        "page_separator_color" = "0.95 0.80 0.80";
-        "status_bar_color" = "0.19 0.20 0.27";
-
-        "font_size" = "${toString sans.size}";
-        "ui_font" = "${mono.family} ${mono.weight}";
-      };
     })
   ]);
 }

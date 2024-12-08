bcc298fea3ae08ec62f2946195ed4ae1a5fedf36
Icy-Thought <icy-thought@pm.me>
2024-12-08 12:30:43 +1030

    update nixfmt package name

--- modules/develop/nix-lang.nix
+++ modules/develop/nix-lang.nix
@@ -3,15 +3,17 @@
 let
   inherit (lib.attrsets) attrValues;
   inherit (lib.modules) mkIf mkMerge;
-in {
-  options.modules.develop.nix = let inherit (lib.options) mkEnableOption;
-  in { enable = mkEnableOption "Nix development" // { default = true; }; };
+in
+{
+  options.modules.develop.nix =
+    let inherit (lib.options) mkEnableOption;
+    in { enable = mkEnableOption "Nix development" // { default = true; }; };
 
   config = mkIf config.modules.develop.nix.enable (mkMerge [
     {
       user.packages = attrValues {
         inherit (pkgs)
-          nixfmt nil # Nix Expression Language
+          nixfmt-rfc-style nil# Nix Expression Language
           manix nix-index nix-init nix-output-monitor nix-tree nix-update
           nixpkgs-review nixpkgs-fmt;
       };
